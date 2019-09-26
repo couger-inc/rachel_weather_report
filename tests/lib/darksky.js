de18f99bb9c46fefb662c36d4eaae830cb5af8fb
TomoyaIgarashi <tomoya@couger.co.jp>
2019-09-26 17:25:47 +0900

    Implement tomorrow weather

--- tests/lib/darksky.js
+++ tests/lib/darksky.js
@@ -1,5 +1,5 @@
 const assert = require('assert').strict;
-const {Darksky, Infrastructure} = require('../../lib/darksky');
+const {Darksky} = require('../../lib/darksky');
 
 describe('Darksky', () => {
   describe('#buildForecastUrl', () => {
@@ -14,23 +14,6 @@ describe('Darksky', () => {
       const darksky = new Darksky(endpoint, apiKey);
       const actual = darksky.buildForecastUrl(latlng, queryParams);
       const expected = 'https://api.darksky.net/forecast/0123456789/35.6828,139.759?units=si&lang=ja';
-      assert.equal(actual, expected);
-    });
-  });
-
-  describe('#generateSpeechMessage', () => {
-    it('Should get message', () => {
-      const nowIcon = 'cloudy';
-      const temperatureHigh = 30.19;
-      const temperatureLow = 24.71;
-      const temperatureNow = 29.92;
-      const actual = Darksky.generateSpeechMessage(
-          nowIcon,
-          temperatureHigh,
-          temperatureLow,
-          temperatureNow);
-      // eslint-disable-next-line max-len
-      const expected = '只今の天気は、くもり。予想最高気温は、30度。最低気温は、25度。現在の気温は、30度です。';
       assert.equal(actual, expected);
     });
   });
